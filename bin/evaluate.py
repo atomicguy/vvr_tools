387fa9a20de650ca78166ff25e6af4f90993e5df
Adam Schuster <adam@atompowered.net>
2018-09-16 14:41:13 +0200

    card and maip iou calculations

--- bin/evaluate.py
+++ bin/evaluate.py
@@ -41,20 +41,35 @@ if __name__ == '__main__':
         y1 = ymin + maip_bbox['y1']
 
         truth = etree.parse(os.path.join(args.truth, '{}.xml'.format(name)))
-        t_xmin = int(truth.find('object').find('bndbox').find('xmin').text)
-        t_xmax = int(truth.find('object').find('bndbox').find('xmax').text)
-        t_ymin = int(truth.find('object').find('bndbox').find('ymin').text)
-        t_ymax = int(truth.find('object').find('bndbox').find('ymax').text)
+        maip = truth.xpath("//*[local-name()='object']/*[text()='MAIP']/./..")[0]
+        t_xmin = int(maip.find('bndbox/xmin').text)
+        t_xmax = int(maip.find('bndbox/xmax').text)
+        t_ymin = int(maip.find('bndbox/ymin').text)
+        t_ymax = int(maip.find('bndbox/ymax').text)
 
-        iou = bb_intersection_over_union((x0, y0, x1, y1), (t_xmin, t_ymin, t_xmax, t_ymax))
+        maip_iou = bb_intersection_over_union((x0, y0, x1, y1), (t_xmin, t_ymin, t_xmax, t_ymax))
 
-        ious.append({'name': name, 'iou': iou})
+        card = truth.xpath("//*[local-name()='object']/*[text()='Card']/./..")[0]
+        c_xmin = int(card.find('bndbox/xmin').text)
+        c_xmax = int(card.find('bndbox/xmax').text)
+        c_ymin = int(card.find('bndbox/ymin').text)
+        c_ymax = int(card.find('bndbox/ymax').text)
+
+        card_iou = bb_intersection_over_union((card_bbox[0], card_bbox[1], card_bbox[2], card_bbox[3]), 
+                                              (c_xmin, c_ymin, c_xmax, c_ymax))
+
+        ious.append({'name': name, 'maip_iou': maip_iou, 'card_iou': card_iou})
 
     with open(os.path.join(args.out, 'ious.json'), 'w') as f:
         json.dump(ious, f, indent=2)
 
-    results = [i['iou'] for i in ious]
-    average_iou = sum(results) / float(len(results))
-    print(average_iou)
+    maip_results = [i['maip_iou'] for i in ious]
+    mean_maip_iou = sum(maip_results) / float(len(maip_results))
+    print('maip iou {}'.format(mean_maip_iou))
 
-    open(os.path.join(args.out, str(average_iou)), 'w').close()
+    card_results = [i['card_iou'] for i in ious]
+    mean_card_iou = sum(card_results) / float(len(card_results))
+    print('card iou {}'.format(mean_card_iou))
+
+    open(os.path.join(args.out, 'maip {}'.format(mean_maip_iou)), 'w').close()
+    open(os.path.join(args.out, 'card {}'.format(mean_card_iou)), 'w').close()
